0c4b11dca2e59127ca8ae02c0a0e18647837e1c8
Visnu <vgcaisse@gmail.com>
2023-04-19 16:46:01 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+//library imports
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//component imports
+import { Navbar } from './components/navbar'
+import { Shop } from './pages/shop/shop'
+import { Cart } from './pages/cart/cart'
+import { Items } from './pages/items/items';
+
+//context provider
+import { ShopContextProvider } from './context/shop-context';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Shop />} />
+            <Route path='/items' element={<Items />} />
+            <Route path='/cart' element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
